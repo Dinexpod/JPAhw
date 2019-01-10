a66c77ab4c9f96aa068b15ab579f631639104cea
Anatolii <dinexpod@gmail.com>
2019-01-10 23:30:27 +0200

    -ended

--- src/main/java/mate/academy/jpahw/patients/PatientService.java
+++ src/main/java/mate/academy/jpahw/patients/PatientService.java
@@ -5,9 +5,7 @@ import mate.academy.jpahw.tests.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,21 +13,17 @@ public class PatientService {
     private static EntityManager em = Main.getEm();
 
     public List<Test> getAllTests() {
-        TypedQuery<Test> query =
-                em
-                        .createQuery("select p from tests p ", Test.class);
-
+        TypedQuery<Test> query = em
+                .createQuery("select p from tests p ", Test.class);
         return query.getResultList();
     }
 
     public List<Test> getAllTestsInDateRange(LocalDateTime from, LocalDateTime to) {
-        TypedQuery<Test> query =
-                em
-                        .createQuery("select p from tests p ", Test.class);
-        List<Test> tests = query.getResultList().stream().filter(test -> (
-                test.getLocalDate().isAfter(from) && test.getLocalDate().isBefore(to)
-        )).collect(Collectors.toList());;
-
-        return tests;
+        TypedQuery<Test> query = em
+                .createQuery("select p from tests p ", Test.class);
+        return query.getResultList()
+                .stream()
+                .filter(test -> (test.getLocalDate().isAfter(from) && test.getLocalDate().isBefore(to)))
+                .collect(Collectors.toList());
     }
 }
